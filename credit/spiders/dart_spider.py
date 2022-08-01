b286d0284e225a26cdc6c7617f2467c147653e50
alsdn472 <alsdn472@gmail.com>
2022-08-01 15:19:48 +0900

    commit

--- credit/spiders/dart_spider.py
+++ credit/spiders/dart_spider.py
@@ -1,5 +1,5 @@
 import scrapy
-
+import time
 
 class DartSpider(scrapy.Spider):
     name = "dart"
@@ -13,12 +13,6 @@ class DartSpider(scrapy.Spider):
             yield scrapy.Request(url = url, callback = self.parse)
 
     def parse(self, response):
-        # css or xpath
-        movie_sels = response.css('ul.lst_detail_t1 > li > dl')
-        item = {}
-        for movie_sel in movie_sels:
-            item['title'] = movie_sel.css('.tit > a::text').get()
-            item['age_limit'] = movie_sel.css('.tit > span::text').get()
-            item['rating'] = movie_sel.css('.star_t1 > a > span.num::text').get()
-            item['rating_count'] = movie_sel.css('.star_t1 > a > span.num2 > em::text').get()
-            yield item
+        time.sleep(3)
+        title = response.css('html > body > p.cover-title > a::text').get()
+        print(title)
